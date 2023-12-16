8cddb7d028015f2a919038b4b971b79050226143
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2023-12-16 01:00:37 -0300

    :lipstick: feat: component navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header"
+import { Navbar } from "./components/Navbar"
 
 function App() {
 
   return (
     <div className="px-8 md:px-28 lg:px-[160px] py-[32px]">
+      <Navbar/>
       <Header/>
     </div>
   )
