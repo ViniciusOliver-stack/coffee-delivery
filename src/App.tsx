2e2318ac007ad3b20fd4564c1bbb318e5f5c3283
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2023-12-15 22:07:36 -0300

    :lipstick: feat: add Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Header } from "./components/Header"
+
 function App() {
 
   return (
-    <>
-    Hello World
-    </>
+    <div className="px-8 md:px-28 lg:px-[160px] py-[32px]">
+      <Header/>
+    </div>
   )
 }
 
