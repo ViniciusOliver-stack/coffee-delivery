53e6d7ccd49fe5ffa71851b6788828b742288584
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2023-12-17 22:49:44 -0300

    :sparkles: feat: create new page home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import { Header } from "../../components/Header"
+import { Coffee } from '../../components/Coffee'
+
+
+export function Home() {
+    return(
+        <>
+            <Header/>
+            <Coffee/>
+        </>
+    )
+}
