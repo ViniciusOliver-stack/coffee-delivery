4816697c4d45a683f6a9d49850c93ae152b02622
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2023-12-17 22:06:48 -0300

    feat: create component Coffee

---
+++ src/components/Coffee/index.tsx
@@ -0,0 +1,17 @@
+import { CoffeeCard } from '../CardCoffee'
+
+export function Coffee() {
+    return(
+        <div className="mt-[140px]">
+            <h3 className="text-3xl font-extrabold text-base-subtitle">Nossos cafés</h3>
+
+            <div className='flex flex-wrap gap-6 items-center justify-center mb-8'>
+                <CoffeeCard/>
+                <CoffeeCard/>
+                <CoffeeCard/>
+                <CoffeeCard/>
+                <CoffeeCard/>
+            </div>
+        </div>
+    )
+}
