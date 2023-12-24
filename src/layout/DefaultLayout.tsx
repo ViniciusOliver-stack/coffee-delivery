25957dc7cb1d08411717fba02c40dc8df188f1a2
ViniciusOliver-stack <vinicius.so.contato@hotmail.com>
2023-12-24 16:46:21 -0300

    :lipstick: feat: spacing adjustment

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -3,7 +3,7 @@ import { Navbar } from "../components/Navbar"
 
 export function DefaultLayout() {
   return (
-    <div className="px-8 md:px-28 lg:px-[160px] py-[32px]">
+    <div className="w-[85%] m-auto py-[32px]">
       <Navbar />
       <Outlet />
     </div>
